26bb67d6b74b43855a84966424d7027b17e0fc18
Hans Pagel <hans.pagel@ueber.io>
2019-01-16 20:28:13 +0100

    initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    lang: 'en-US',
+    title: 'scrumpy/tiptap',
+    themeConfig: {
+        docsRepo: 'scrumpy/tiptap-docs',
+        editLinks: true,
+        editLinkText: 'Edit this page on GitHub',
+        search: true,
+        sidebar: {},
+        nav: [
+            { text: 'GitHub', link: 'https://github.com/scrumpy/tiptap' },
+            { text: 'Examples', link: 'https://tiptap.scrumpy.io/' },
+        ],
+    },
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Documentation
+
+🚧 Work in Progress
